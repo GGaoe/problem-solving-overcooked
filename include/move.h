48781a8a47ad2a7a6b840b806b960a837643a948
Gaoge <2240378397@qq.com>
2024-05-20 20:50:48 +0800

    1

--- include/move.h
+++ include/move.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <framework.h>
+#include <queue>
 bool dis();
 bool action(std::string s);
 bool in(double x,double y,double d_x,double d_y);
--- source/move.cpp
+++ source/move.cpp
@@ -6,6 +6,9 @@
 #include <framework.h>
 #include <math.h>
 #include <move.h>
+#include <stack>
+#include <cmath>
+#include <queue>
 
 extern Player Players[2+5];
 extern int width, height;
@@ -21,16 +24,27 @@ extern struct Order Order[20 + 5];
 extern int entityCount;
 extern struct Entity Entity[20 + 5];
 extern int status;
+
+char bfs_map[20+5][20+5];
+bool lable[20+5][20+5];
+int father[20+5][20+5];
+
 //double error_d=0.06;//允许的误差范围
 //double error_u=0.96;//NUM=1,2226 points //NUM=2,2747
 //double error_d=0.05;//允许的误差范围
 //double error_u=0.989;//NUM=3,2681 points
 double error_d1=0.4;//允许的误差范围
 double error_u1=0.6;//NUM=3,2681 points
+//double error_d=0.3;//允许的误差范围
+//double error_u=0.7;//NUM=4,2103
 double error_d=0.2;//允许的误差范围
 double error_u=0.8;//NUM=4,2103
 //double error_d=0.2;//允许的误差范围
 //double error_u=0.954;//NUM=4,2103
+
+int f_x[4]={0,1,0,-1};
+int f_y[4]={1,0,-1,0};
+
 
 bool dis(){
     double x1=Players[0].x;
@@ -43,6 +57,68 @@ bool action(std::string s){
     if(s=="Move U"||s=="Move D"||s=="Move L"||s=="Move R"||s=="Move RU"||s=="Move RD"||s=="Move LU"||s=="Move LD")
     return 1;
     return 0;
+}
+
+void map_copy(){
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+        bfs_map[i][j]=Map[i][j];
+        lable[i][j]=0;
+        father[i][j]=0;
+        }
+    }
+    return;
+}
+//地图的遍历为y，x；player0优先使用地图，player1遍历的时候留下0的位置，给1让路
+std::string bfs(int x,int y,int* dx,int* dy,int id){
+    map_copy();
+    bfs_map[y][x]=id+'0';
+    std::queue<int>q_x;
+    std::queue<int>q_y;
+    q_x.push(x);
+    q_y.push(y);
+    int find=0;
+    while(!q_x.empty()&&!find){
+        int tmp_x=q_x.front();
+        int tmp_y=q_y.front();
+        q_x.pop();
+        q_y.pop();
+        lable[tmp_y][tmp_x]=1;
+        for(int i=0;i<4;i++){
+            if(tmp_x+f_x[i]==*dx&&tmp_y+f_y[i]==*dy){
+                father[*dy][*dx]=i;
+                find=1;
+                break;
+            }
+            else{
+                if(bfs_map[tmp_y+f_y[i]][tmp_x+f_x[i]]=='.'&&!lable[tmp_y+f_y[i]][tmp_x+f_x[i]]){
+                    father[tmp_y+f_y[i]][tmp_x+f_x[i]]=i;
+                    lable[tmp_y+f_y[i]][tmp_x+f_x[i]]=1;
+                    q_x.push(tmp_x+f_x[i]);
+                    q_y.push(tmp_y+f_y[i]);
+                }
+            }
+        }
+    }
+    if(find){
+        int tmp_x=*dx;
+        int tmp_y=*dy;
+        int f=father[*dy][*dx];
+        while(tmp_x!=x||tmp_y!=y){
+            f=father[tmp_y][tmp_x];
+            std::cerr<<tmp_y<<" "<<tmp_x<<"->";
+            int f_1=f_x[father[tmp_y][tmp_x]];
+            int f_2=f_y[father[tmp_y][tmp_x]];
+            tmp_x-=f_1;
+            tmp_y-=f_2;
+        }
+        //fix ther destination
+        std::cerr<<"fix"<<x<<" "<<y<<" "<<x+f_x[f]<<" "<<y+f_y[f]<<std::endl;
+        *dx=x+f_x[f];
+        *dy=y+f_y[f];
+    }
+    else return "Move";
+    return "Move";
 }
 
 bool in(double x,double y,double d_x,double d_y){
@@ -58,7 +134,6 @@ bool hard_in(double x,double y,double d_x,double d_y){
     }
     return 0;
 }//检查是否抵达目的地
-
 
 std::string inte(double d_x, double d_y,  int op){
     if(d_x==0){
@@ -97,18 +172,34 @@ std::string inte(double d_x, double d_y,  int op){
 }
 
 std::string movement(double d_x,double d_y,int id){
+    int x=(int)Players[id].x;
+    int y=(int)Players[id].y;
+    int des_xx=lround(d_x);
+    int des_yy=lround(d_y);
+    //std::cerr<<Players[id].x<<" "<<Players[id].y<<std::endl;
+    if(des_xx!=x||des_yy!=y)bfs(x,y,&des_xx,&des_yy,id);
+    d_x=des_xx;
+    d_y=des_yy;
+    /*
+     for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+        std::cerr<<bfs_map[i][j];
+        }
+        std::cerr<<std::endl;
+    }*/
+
     std::string ret = "Move ";
     if (Players[id].x < d_x + error_d)
         ret += "R";
-    if (Players[id].x > d_x+error_u)
+    else if (Players[id].x > d_x+error_u)
         ret += "L";
     if (Players[id].y < d_y + error_d)
         ret += "D";
-    if (Players[id].y > d_y+error_u)
+    else if (Players[id].y > d_y+error_u)
         ret += "U";
     if(ret=="Move ")ret="Move";
-    if(Players[id].y>7 && Players[id].y<8 && Players[id].x>7.8 && ret=="Move LD")ret="Move L";
-    if(Players[id].x>7 && Players[id].x<8 && Players[id].y>7.8 && ret=="Move RU")ret="Move U";
+    if(Players[id].y>6 && Players[id].y<9 && Players[id].x>7 && (ret=="Move LD"||ret=="Move D"))ret="Move L";
+    if(Players[id].x>6 && Players[id].x<9 && Players[id].y>7 && (ret=="Move RU"||ret=="Move R"))ret="Move U";
     return ret;
 }//移动策略
 
