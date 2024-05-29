fc39ba9b17fa9e414029dae78221d1681fbc1128
Gaoge <2240378397@qq.com>
2024-05-30 00:21:12 +0800

    1

--- source/main.cpp
+++ source/main.cpp
@@ -441,7 +441,7 @@ int main()
     //     }
     //     else player0_Action="Move";
     // }
-        player0_Action=movement(5,3,0);//移动操作
+        player0_Action=movement(3,3,0);//移动操作
      }
 
     //player1负责拿脏盘子以及洗盘子
